df23e1d2aa89c95720294a93da933c5e258510f3
GrafZeppelin <grafzeppelin@gamlo.de>
2021-03-17 18:50:18 +0100

    include dht sensor

--- lib/MQ135/MQ135.h
+++ lib/MQ135/MQ135.h
@@ -25,7 +25,7 @@ v1.0 - First release
 #define RLOAD 10.0
 /// Calibration resistance at atmospheric CO2 level
 // #define RZERO 76.63
-#define RZERO 1
+#define RZERO 1357.29
 /// Parameters for calculating ppm of CO2 from sensor resistance
 #define PARA 116.6020682
 #define PARB 2.769034857
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,64 @@
 #include <Arduino.h>
 #include <MQ135.h>
+#include <dht.h>
+
+#define RZERO 1
 
 #define PIN_MQ135 A0
-MQ135 mq135_sensor = MQ135(PIN_MQ135);
+#define PIN_DHT11 A1
+
+MQ135 co2_sensor = MQ135(PIN_MQ135);
+dht dht_sensor;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
+  Serial.print(RZERO);
+  Serial.println("---------------------------");
+  Serial.print("DHT:\t");
+  int chk = dht_sensor.read11(PIN_DHT11);
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+      Serial.print("OK,\t"); 
+      break;
+    case DHTLIB_ERROR_CHECKSUM: 
+      Serial.print("Checksum error,\t"); 
+      break;
+    case DHTLIB_ERROR_TIMEOUT: 
+      Serial.print("Time out error,\t"); 
+      break;
+    case DHTLIB_ERROR_CONNECT:
+        Serial.print("Connect error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_L:
+        Serial.print("Ack Low error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_H:
+        Serial.print("Ack High error,\t");
+        break;
+    default: 
+      Serial.print("Unknown error,\t"); 
+      break;
+  }
+  Serial.println();
+
+  Serial.print ("temperature: ");
+  Serial.println (dht_sensor.temperature);
+  Serial.print ("humidity: ");
+  Serial.println (dht_sensor.humidity);
+
   float val = analogRead(A0);
   Serial.print ("raw = ");
   Serial.println (val);
-  float zero = mq135_sensor.getRZero();
+  float zero = co2_sensor.getCorrectedRZero(dht_sensor.temperature, dht_sensor.humidity);
   Serial.print ("rzero: ");
   Serial.println (zero);
-  float ppm = mq135_sensor.getPPM();
+  float ppm = co2_sensor.getCorrectedPPM(dht_sensor.temperature, dht_sensor.humidity);
   Serial.print ("ppm: ");
   Serial.println (ppm);
+
   delay(2000);
 }
