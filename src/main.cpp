5a16fccbad0b86ed52d2d709c107c8dccd064172
GrafZ3pp3lin <grafzeppelin@gamlo.de>
2021-07-14 19:42:59 +0200

    use arduino uno

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <MQ135.h>
 #include <Adafruit_BME280.h>
 
-#define PIN_MQ135 A7
+#define PIN_MQ135 A0
 #define PIN_NOISE 2
 #define PIN_LED_GREEN 3
 #define PIN_LED_GREEN2 4
@@ -27,7 +27,17 @@ void loop() {
   // float humidity = bme.readHumidity();
 
   // float r0 = co2_sensor.getCorrectedR0(temp, humidity);
+  float in = analogRead(PIN_MQ135);
+  double v = co2_sensor.getVoltage();
+  double r = co2_sensor.getResistance();
   float r0 = co2_sensor.getR0();
+
+  Serial.print("Analog Read: ");
+  Serial.println(in);
+  Serial.print("Voltage: ");
+  Serial.println(v);
+  Serial.print("Resistance: ");
+  Serial.println(r);
 
   // Serial.print("Temperature: ");
   // Serial.println(temp);
@@ -35,6 +45,8 @@ void loop() {
   // Serial.println(humidity);
   Serial.print("R0: ");
   Serial.println(r0);
+
+  Serial.println("-----------------------------");
 
   delay(1000);
 }
